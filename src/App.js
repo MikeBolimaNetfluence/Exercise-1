ddfc20c42c3901fca85babe55bf331d96f06abec
ShaireenManzano-16 <smanzano@forbescollege.org>
2022-06-24 16:32:58 +0800

    Workshop_Activity

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "./index.css";
+
+import TopicBrowser from "./components/TopicBrowser/TopicBrowser";
+
+const App = () => {
+  return <TopicBrowser />;
+};
+
+export default App;
